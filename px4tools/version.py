233aea402fc30bff3beb4e5deb95082df6618046
James Goppert <james.goppert.gmail.com>
2015-10-18 21:31:55 -0400

    Update travis.

--- px4tools/version.py
+++ px4tools/version.py
@@ -3,7 +3,7 @@
 short_version = '0.1.1'
 version = '0.1.1'
 full_version = '0.1.1'
-git_revision = 'fcdbeb4f66027bdff83c78aadca237c14cbdfdc6'
+git_revision = '78839ecdd31ed33fc98981f2bf13a278acf9b24e'
 release = True
 
 if not release:

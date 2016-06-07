67bc6574036159b2c65b8fe0df0988c16f1bbd86
James Goppert <james.goppert@gmail.com>
2016-06-07 01:48:47 -0500

    Fixed meta.yaml.

--- conda_build.sh
+++ conda_build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+bdir=~/anaconda3/conda-bld
+conda config --set anaconda_upload no
 conda build .
-conda convert --platform all ~/anaconda3/conda-bld/linux-64/px4tools*.tar.bz2 -o ~/anaconda3/conda-bld
+conda convert --platform all $bdir/linux-64/px4tools*.tar.bz2 -o ~/anaconda3/conda-bld
+anaconda upload -u dronecrew $bdir/linux-64/*
+anaconda upload -u dronecrew $bdir/win-32/*
+anaconda upload -u dronecrew $bdir/win-64/*
+anaconda upload -u dronecrew $bdir/osx-64/*
 

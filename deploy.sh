a2b40d343364bec6d3659931351637bdae2f068e
James Goppert <james.goppert@gmail.com>
2016-06-27 13:05:02 -0400

    Work on conda.

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,6 @@
 root=$PWD
 set -e
 conda install conda-build anaconda-client anaconda-build
-anaconda login
 python --version
 bdir=~/anaconda3/conda-bld
 rm -rf $bdir
@@ -11,10 +10,12 @@ conda config --set anaconda_upload no
 cd conda
 conda build .
 anaconda build submit .
-conda convert --platform all $bdir/linux-64/px4tools*.tar.bz2 -o ~/anaconda3/conda-bld
+anaconda build tail -f dronecrew/px4tools 2
+#conda convert --platform all $bdir/linux-64/px4tools*.tar.bz2 -o ~/anaconda3/conda-bld
 anaconda upload -u dronecrew $bdir/linux-64/px4tools*.tar.bz2
-anaconda upload -u dronecrew $bdir/win-32/px4tools*.tar.bz2
-anaconda upload -u dronecrew $bdir/win-64/px4tools*.tar.bz2
-anaconda upload -u dronecrew $bdir/osx-64/px4tools*.tar.bz2
+#anaconda upload -u dronecrew $bdir/win-32/px4tools*.tar.bz2
+#anaconda upload -u dronecrew $bdir/win-64/px4tools*.tar.bz2
+#anaconda upload -u dronecrew $bdir/osx-64/px4tools*.tar.bz2
+anaconda upload -u dronecrew $bdir/noarch/px4tools*.tar.bz2
 cd $root
 python setup.py sdist upload

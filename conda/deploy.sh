d8d8d107463d453e601512ab8cdbdcee379b3ae3
James Goppert <james.goppert@gmail.com>
2016-06-27 18:29:55 -0400

    Move deploy.

---
+++ conda/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+root=$PWD
+set -e
+conda install conda-build anaconda-client anaconda-build
+python --version
+bdir=~/anaconda3/conda-bld
+rm -rf $bdir
+conda config --set anaconda_upload no
+conda build .
+anaconda build submit .
+anaconda build tail -f dronecrew/px4tools 2
+conda convert --platform all $bdir/linux-64/px4tools*.tar.bz2 -o ~/anaconda3/conda-bld
+anaconda upload -u dronecrew $bdir/linux-64/px4tools*.tar.bz2
+anaconda upload -u dronecrew $bdir/win-32/px4tools*.tar.bz2
+anaconda upload -u dronecrew $bdir/win-64/px4tools*.tar.bz2
+anaconda upload -u dronecrew $bdir/osx-64/px4tools*.tar.bz2
+#anaconda upload -u dronecrew $bdir/noarch/px4tools*.tar.bz2

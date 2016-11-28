788bf8a4766d8c6ff28260e19f322525ffb30b90
James Goppert <james.goppert@gmail.com>
2016-11-28 11:39:43 -0500

    Added warnings for uninstalled modules, also bumped version.

--- px4tools/__init__.py
+++ px4tools/__init__.py
@@ -2,6 +2,38 @@ from __future__ import print_function
 
 try:
     from .mapping import *
+except ImportError as ex:
+    print(ex)
+    print('''
+WARNING: mapping module requires basemap
+    sudo apt-get install python-basemap
+    or:
+    conda install basemap
+''')
+
+try:
     from .analysis import *
-except ImportError as e:
-    print(e)
+except ImportError as ex:
+    print(ex)
+
+try:
+    from .ulog import *
+except ImportError as ex:
+    print(ex)
+    print('''
+WARNING: ulog module requires pyulog
+
+please install:
+    pip install pyulog
+''')
+
+try:
+    from .logsysid import *
+except ImportError as ex:
+    print(ex)
+    print('''
+WARNING: logsysid module requires python-control
+
+please install:
+    pip install control
+''')
